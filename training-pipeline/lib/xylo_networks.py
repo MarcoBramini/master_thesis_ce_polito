c757adf771f09e6e3f67114bc1a818e3ae0d189e
Marco Bramini <braminim@gmail.com>
2023-11-13 10:57:59 +0100

    Update Xylo networks

--- training-pipeline/lib/xylo_networks.py
+++ training-pipeline/lib/xylo_networks.py
@@ -2,12 +2,23 @@ from rockpool.nn.modules.torch import LinearTorch, LIFTorch
 from rockpool.nn.combinators import Sequential, Residual
 
 
-def get_rec_deep(n_input_channels, n_population, n_output_channels, neuron_parameters):
+def get_rec_simple(
+    n_input_channels, n_population, n_output_channels, neuron_parameters
+):
     return Sequential(
         LinearTorch((n_input_channels, n_population)),
         LIFTorch(n_population, has_rec=True, **neuron_parameters),
-        LinearTorch((n_population, n_population)),
+        LinearTorch((n_population, n_output_channels)),
+        LIFTorch(n_output_channels, **neuron_parameters),
+    )
+
+
+def get_rec_deep(n_input_channels, n_population, n_output_channels, neuron_parameters):
+    return Sequential(
+        LinearTorch((n_input_channels, n_population)),
         LIFTorch(n_population, **neuron_parameters),
+        LinearTorch((n_population, n_population)),
+        LIFTorch(n_population, has_rec=True, **neuron_parameters),
         LinearTorch((n_population, n_output_channels)),
         LIFTorch(n_output_channels, **neuron_parameters),
     )
@@ -33,58 +44,34 @@ def get_ff_deep(n_input_channels, n_population, n_output_channels, neuron_parame
     )
 
 
-def get_ff_deep_deep(n_input_channels, n_population, n_output_channels, neuron_parameters):
-    return Sequential(
-        LinearTorch((n_input_channels, n_population)),
-        LIFTorch(n_population, **neuron_parameters),
-        LinearTorch((n_population, n_population)),
-        LIFTorch(n_population, **neuron_parameters),
-        LinearTorch((n_population, n_population)),
-        LIFTorch(n_population, **neuron_parameters),
-        LinearTorch((n_population, n_output_channels)),
-        LIFTorch(n_output_channels, **neuron_parameters),
-    )
-
-
-def get_ff_deep_deep_deep(n_input_channels, n_population, n_output_channels, neuron_parameters):
-    return Sequential(
-        LinearTorch((n_input_channels, n_population)),
-        LIFTorch(n_population, **neuron_parameters),
-        LinearTorch((n_population, n_population)),
-        LIFTorch(n_population, **neuron_parameters),
-        LinearTorch((n_population, n_population)),
-        LIFTorch(n_population, **neuron_parameters),
-        LinearTorch((n_population, n_population)),
-        LIFTorch(n_population, **neuron_parameters),
-        LinearTorch((n_population, n_output_channels)),
-        LIFTorch(n_output_channels, **neuron_parameters),
-    )
-
-
-def get_ff_deep_res(n_input_channels, n_population, n_output_channels, neuron_parameters):
+def get_ff_deep_res(
+    n_input_channels, n_population, n_output_channels, neuron_parameters
+):
     return Sequential(
         LinearTorch((n_input_channels, n_population)),
         LIFTorch(n_population, **neuron_parameters),
         Residual(
             LinearTorch((n_population, n_population)),
-            LIFTorch(n_population, **neuron_parameters)
+            LIFTorch(n_population, **neuron_parameters),
         ),
         LinearTorch((n_population, n_output_channels)),
         LIFTorch(n_output_channels, **neuron_parameters),
     )
 
 
-def get_ff_deep_deep_res(n_input_channels, n_population, n_output_channels, neuron_parameters):
+def get_ff_deep_deep_res(
+    n_input_channels, n_population, n_output_channels, neuron_parameters
+):
     return Sequential(
         LinearTorch((n_input_channels, n_population)),
         LIFTorch(n_population, **neuron_parameters),
         Residual(
             LinearTorch((n_population, n_population)),
-            LIFTorch(n_population, **neuron_parameters)
+            LIFTorch(n_population, **neuron_parameters),
         ),
         Residual(
             LinearTorch((n_population, n_population)),
-            LIFTorch(n_population, **neuron_parameters)
+            LIFTorch(n_population, **neuron_parameters),
         ),
         LinearTorch((n_population, n_output_channels)),
         LIFTorch(n_output_channels, **neuron_parameters),
